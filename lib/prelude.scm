695eaf60415d9be3177947a8582427f1caa9e53f
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2022-10-25 18:33:44 +0300

    Add for-each

--- lib/prelude.scm
+++ lib/prelude.scm
@@ -10,3 +10,15 @@
                         ls
                         (error "map: improper list" list1))))))
       (map proc list1))))
+
+(define for-each
+  (lambda (proc list1)
+    (letrec ((for-each (lambda (f ls)
+                         (if (pair? ls)
+                           (begin
+                             (f (car ls))
+                             (for-each f (cdr ls)))
+                           (if (null? ls)
+                             ls
+                             (error "for-each: improper list" list1))))))
+      (for-each proc list1))))
