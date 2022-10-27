a39907d3578c68941f868a9638ee9c3a903aae4f
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2022-10-27 12:23:55 +0300

    assert_vector_equal

--- tests/special_forms.rs
+++ tests/special_forms.rs
@@ -6,6 +6,8 @@ use molysite::verifier::verify;
 use molysite::oref::{ORef, Fixnum};
 use molysite::string::String;
 use molysite::symbol::Symbol;
+use molysite::vector::Vector;
+use molysite::heap_obj::Indexed;
 
 fn eval_string(mt: &mut Mutator, s: &str) -> ORef {
     let mut reader = Reader::new(s);
@@ -27,14 +29,31 @@ fn eval_string(mt: &mut Mutator, s: &str) -> ORef {
     v
 }
 
+fn assert_vector_equal(mt: &Mutator, v1: ORef, v2: ORef) -> bool {
+    let v1 = v1.try_cast::<Vector<ORef>>(mt).unwrap();
+    let v2 = v2.try_cast::<Vector<ORef>>(mt).unwrap();
+
+    unsafe {
+        assert_eq!(v1.as_ref().indexed_field().len(), v2.as_ref().indexed_field().len());
+
+        for (v, u) in v1.as_ref().indexed_field().iter().zip(v2.as_ref().indexed_field().iter()) {
+            assert_eq!(v, u);
+        }
+    }
+
+    true
+}
+
 #[test]
-fn quote() {
+fn test_quote() {
     let mut mt = Mutator::new(1 << 20, false).unwrap();
 
     assert_eq!(eval_string(&mut mt, "(quote a)"), Symbol::new(&mut mt, "a").into());
-    assert_eq!(
-        eval_string(&mut mt, "(quote #(a b c))"),
-        *Reader::new("#(a b c)").next(&mut mt).unwrap().unwrap().v);
+
+    let v1 = eval_string(&mut mt, "(quote #(a b c))");
+    let v2 = *Reader::new("#(a b c)").next(&mut mt).unwrap().unwrap().v;
+    assert_vector_equal(&mt, v1, v2);
+
     assert_eq!(
         eval_string(&mut mt, "(quote (+ 1 2))"),
         *Reader::new("(+ 1 2)").next(&mut mt).unwrap().unwrap().v);
