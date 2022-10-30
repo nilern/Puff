f7d4dd5903f98147bdc34e6d7950a32b625bcb1b
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2022-10-30 19:18:27 +0200

    Fix `map` for lists of > 1 elements

--- lib/prelude.scm
+++ lib/prelude.scm
@@ -5,19 +5,17 @@
 (define map
   (lambda (proc list1)
     (if (pair? list1)
-      (letrec ((map-tail! (lambda (ls last-pair)
+      (letrec ((ls* (cons (proc (car list1)) '()))
+               (map-tail! (lambda (ls last-pair)
                             (if (pair? ls)
                               (letrec ((pair (cons (proc (car ls)) '())))
                                 (begin
-                                  (set-cdr! last-pair (proc (car ls)))
+                                  (set-cdr! last-pair pair)
                                   (map-tail! (cdr ls) pair)))
                               (if (null? ls)
-                                ls
-                                (error "map: improper list" list1)))))
-               (ls* (cons (proc (car list1)) '())))
-        (begin
-          (map-tail! (cdr list1) ls*)
-          ls*))
+                                ls*
+                                (error "map: improper list" list1))))))
+        (map-tail! (cdr list1) ls*))
       (if (null? list1)
         list1
         (error "map: improper list" list1)))))
